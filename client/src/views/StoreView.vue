f3bc88eda15ae8b71c56f49a8ea6254b4bd0b2d9
vincen77 <72100032+vincen77@users.noreply.github.com>
2022-10-13 15:15:28 -0400

    oct 13 work

---
+++ client/src/views/StoreView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1 class="title">Store View</h1>
+        <p>Store View</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
