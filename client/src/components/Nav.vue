4576670081e924443ccef78a75f5573656424be7
vincen77 <72100032+vincen77@users.noreply.github.com>
2022-11-14 15:15:13 -0500

    catching up to class

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
     import { ref } from 'vue';
     import { RouterLink } from 'vue-router';
+    import Cart from './Cart.vue';
     import LoginBadge from './LoginBadge.vue';
 
-    let isActive = ref(false);
+    const isActive = ref(false);
+    const isCartOpen = ref(false);
 
 </script>
 
 <template>
+    <Cart :is-open="isCartOpen" />
     <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
 
         <div class="container">
@@ -29,8 +32,8 @@
               Home
             </router-link>
       
-            <router-link class="navbar-item" to="/store">
-              Store
+            <router-link class="navbar-item" to="/products">
+              Products
             </router-link>
       
             <div class="navbar-item has-dropdown is-hoverable">
@@ -57,6 +60,11 @@
           </div>
       
           <div class="navbar-end">
+            <div class="navbar-item">
+                <button class="button is-primary" @click="isCartOpen = !isCartOpen">
+                  <strong>Cart</strong>
+                </button>
+              </div>
             <div class="navbar-item">
 
               <login-badge></login-badge>
